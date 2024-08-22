b8ab1da50c62636cb49998d31f588dd27fb368d5
r all <rmall21@outlook.com>
2024-08-21 19:16:47 -0500

    opps3.sh

---
+++ opps3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Conditional Statment 
+# This ops challenges is about conditinal stamtment of if else statments and how they work
+# We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
+
+number=4
+if [ "$number" -gt 6 ]; then
+    echo "$number is greater than 5."
+elif [ "$number" -lt 6 ]; then
+    echo "$number is less than 6"
+else 
+    echo "$number is exactly 6"
+fi
+
